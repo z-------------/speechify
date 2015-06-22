5927d026c9297b81a2e55c53f51add19534c2150
Zack Guard <zacharyguard@gmail.com>
2015-06-22 20:22:17 +0800

    Split up long utterances
    Chrome stops reading after about 250 chars

--- background.js
+++ background.js
@@ -1,12 +1,30 @@
 var startReading = function(response) {
-    var ttsOptions = {};
+    var ttsOptions = {
+        enqueue: true
+    };
+    var text = response.text;
+    var splitLength = 200;
+    var utterances = [];
+    var words = text.split(" ");
+
     if (response.language) {
         ttsOptions.lang = response.language;
     }
 
-    chrome.tts.speak(response.text, ttsOptions);
+    for (var w = 0; w < words.length; w++) {
+        if (utterances.length === 0 || utterances[utterances.length - 1].length + words[w].length > splitLength) {
+            utterances.push("");
+            console.log(utterances, words[w]);
+        }
+        utterances[utterances.length - 1] += words[w] + " ";
+    }
 
     console.log("speechify reading: '" + response.text + "' with options ", ttsOptions);
+    console.log(utterances);
+
+    utterances.forEach(function(text) {
+        chrome.tts.speak(text, ttsOptions);
+    });
 };
 
 var getTextInfo = function(tab, callback) {
